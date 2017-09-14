3517f68ca7206d6d86d78e21e603227fe4b132fc
ynag <yang@yang.com>
2017-09-14 10:13:49 +0800

    update

---
+++ module1.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Administrator
+* @Date:   2017-09-13 17:59:09
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-09-14 10:05:53
+*/
+// export var a = 'a'
+// export var b = 'b'
+// 同
+// var a = 'a',b = 'b'；
+// f () {}
+// export { a,b,f }//输出一组变量
